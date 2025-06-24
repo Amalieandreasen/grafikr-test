1806feee36512d8d956a53a6ffb90ea206f64a3a
Amalie <amalie.andreasen99@hotmail.com>
2025-06-24 22:31:29 +0200

    halvdelen af siden sat op

--- grafikr/src/main.ts
+++ grafikr/src/main.ts
@@ -1,2 +1,26 @@
-import './style.css'
+import './tailwind.css'
+
+const hamburger = document.querySelector(".hamburger-btn");
+const navMenu = document.querySelector(".navmenu");
+const header = document.querySelector("header")
+
+hamburger?.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".navlink").forEach(n => n.addEventListener("click", () => {
+    hamburger?.classList.remove("active")
+    navMenu?.classList.remove("active")
+}));
+
+window.addEventListener("scroll", () => {
+    if(window.scrollY > 10){
+        header?.classList.remove("bg-grafikGrey")
+        header.classList.add("bg-white/70", "backdrop-blur-md",);
+    } else {
+      header.classList.remove("bg-white/70", "backdrop-blur-md");
+      header.classList.add("bg-grafikGrey");
+    }
+})
 
